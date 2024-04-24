99500d426828945d9aec4bf6543011895ed1941e
KendleMintJed <jamie.astonfamily@gmail.com>
2024-04-24 10:17:52 +0100

    Set scrolloff option

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,9 @@ require "nvchad.options"
 
 local o = vim.o
 
-o.cursorlineopt ='both'
+o.cursorlineopt = "both"
 vim.opt.relativenumber = true
 vim.opt.foldmethod = "marker"
 vim.opt.foldmarker = "{,}"
 vim.opt.foldlevel = 100
+vim.opt.scrolloff = 5
